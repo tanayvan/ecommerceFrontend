012b58226ceb2ee4272d72a628511d2767995b20
Tanay Van <tanayvan258@gmail.com>
2020-10-13 17:21:13 +0530

    bug fixes

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -3,41 +3,12 @@ import {
   CART_LIST_FAIL,
   CART_LIST_REQUEST,
   CART_LIST_SUCCESS,
-  ADD_TO_CART_FAIL,
-  ADD_TO_CART_REQUEST,
-  ADD_TO_CART_SUCCESS,
 } from "../constants/cartConstants";
 
 export const getCartData = () => async (dispatch) => {
   try {
     dispatch({ type: CART_LIST_REQUEST });
     const { data } = await Axios.get("/api/getcart/admin");
-    dispatch({
-      type: CART_LIST_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: CART_LIST_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
-
-export const addtocart = (id, size, user) => async (dispatch) => {
-  try {
-    dispatch({ type: ADD_TO_CART_FAIL });
-    const { data } = await Axios.post(
-      "/api/addtocart/admin",
-      (data = {
-        product: { _id: id },
-        size: size,
-        user: user,
-      })
-    );
     dispatch({
       type: CART_LIST_SUCCESS,
       payload: data,
